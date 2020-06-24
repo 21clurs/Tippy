da926834d45e57d8d872c04b1c85f83dba438b96
Clara Kim <claraj@fb.com>
2020-06-24 00:24:49 -0400

    Added animations and bill amount set as first responder

--- Tippy/ViewController.m
+++ Tippy/ViewController.m
@@ -13,6 +13,7 @@
 @property (weak, nonatomic) IBOutlet UITextField *billField;
 @property (weak, nonatomic) IBOutlet UILabel *tipLabel;
 @property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@property (weak, nonatomic) IBOutlet UILabel *billLabel;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *tipControl;
 
 @end
@@ -22,6 +23,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+}
+
+- (void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    
+    // Make the billField first responder
+    [self.billField becomeFirstResponder];
 }
 
 - (IBAction)onTap:(id)sender {
@@ -43,6 +51,39 @@
     
     self.tipLabel.text = [NSString stringWithFormat:@"$%.2f", tip];
     self.totalLabel.text = [NSString stringWithFormat:@"$%.2f", total];
+}
+- (IBAction)onEditBegin:(id)sender {
+    NSLog(@"here");
+    [UIView animateWithDuration:0.2 animations:^{
+        self.billField.frame = CGRectMake(self.billField.frame.origin.x, self.billField.frame.origin.y+10, self.billField.frame.size.width, self.billField.frame.size.height);
+        self.billLabel.frame = CGRectMake(self.billLabel.frame.origin.x, self.billLabel.frame.origin.y+10, self.billLabel.frame.size.width, self.billLabel.frame.size.height);
+        //[self.billField setFont:[UIFont systemFontOfSize:40]];
+    }];
+    /*
+    [UIView animateWithDuration:1 animations:^{
+        self.tipLabel.alpha=0;
+        self.totalLabel.alpha=0;
+    }];
+     */
+}
+- (IBAction)onEditEnd:(id)sender {
+    NSLog(@"no here");
+    CGRect newFrame = self.billField.frame;
+    CGRect newLabelFrame = self.billLabel.frame;
+    newFrame.origin.y -=10;
+    newLabelFrame.origin.y -=10;
+    
+    [UIView animateWithDuration:0.2 animations:^{
+        self.billField.frame = newFrame;
+        self.billLabel.frame = newLabelFrame;
+        //[self.billField setFont:[UIFont systemFontOfSize:14]];
+    }];
+    /*
+    [UIView animateWithDuration:1 animations:^{
+        self.tipLabel.alpha=1;
+        self.totalLabel.alpha=1;
+    }];
+     */
 }
 
 @end
